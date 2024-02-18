b622d55dcbb5b3184c4194018ee954eaaefe631b
Sheroz Akram <sheroz.akram@outlook.com>
2024-02-18 20:31:17 +0500

    flutter project created

---
+++ Front End/vivid_estate_frontend_flutter/android/app/src/main/kotlin/com/example/vivid_estate_frontend_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vivid_estate_frontend_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Front End/vivid_estate_frontend_flutter/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vivid Estate',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // Main Data of our home page
+        body: Center(
+      child: Container(
+        margin: const EdgeInsets.only(top: 20.0),
+        child: Column(children: <Widget>[
+          // Application Logo
+          Image.asset(
+            "assets/logo.png",
+            width: MediaQuery.of(context).size.width * 0.50,
+          ),
+
+          // Application Name
+          const Text("Vivid Estate",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 36.0,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF006E86),
+                fontFamily: "Berlin Sans", // Use a standard font
+              )),
+
+          // Application Text
+          const Text("Your Path to Real Residence",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF5093A1),
+                fontFamily: "Berlin Sans", // Use a standard font
+              ))
+        ]),
+      ),
+    ));
+  }
+}
